a892fb0433b26038168cd98329c67b1806f9dd2b
Nareshd <dubbakanaresh11.dn@gmail.com>
2026-03-17 11:49:17 +0530

    yml testing 3

--- tests/Myprojects.spec.ts
+++ tests/Myprojects.spec.ts
@@ -105,7 +105,7 @@ test.skip("Verify the project creation with valid credentials",async ({myProject
 })
 
 
-test("Verify the project creation with Already exits project",async ({myProjectsPage})=>
+test.only("Verify the project creation with Already exits project",async ({myProjectsPage})=>
 {
 
     await myProjectsPage.clickOnNewProjectBtn();
