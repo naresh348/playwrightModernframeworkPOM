3a486ae30ba644094e0ad0c79668b18ebae8b7bc
Nareshd <dubbakanaresh11.dn@gmail.com>
2026-03-23 17:41:31 +0530

    Practice  sections

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
 
   //retries: process.env.CI ? 2 : 0,
-  retries:1,
+  retries: 1,
   workers: process.env.CI ? 1 : undefined,
 
   outputDir: 'test-results/',
@@ -42,7 +42,7 @@ export default defineConfig({
 
       use: {
         browserName: 'chromium'
-       // ...devices['iPhone 12 Mini']
+        // ...devices['iPhone 12 Mini']
       }
     }
 
